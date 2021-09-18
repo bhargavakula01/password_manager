f1d16f4bc6635b98d04a30cd1222e89405d58ee0
Bhargav Akula <bhargav.akula1@gmail.com>
2021-09-18 15:40:39 +0000

    updated code

--- password-hasher/hashpasswords.py
+++ password-hasher/hashpasswords.py
@@ -32,6 +32,26 @@ def read_from_file(url):
 		if(not found):
 			print('url not found')
 def updating_from_file(url):
+	readFile = open('password.json','r')
+	inputpass = input("Enter new password that you want to hash")
+	index_password = 0
+	passwordlist = readFile.readlines()
+	readFile.close()
+	
+	shaobj = sha256()
+	encode = inputpass.encode()
+	hashed = shaobj.hexdigest()
+
+	writefile = open('password.json', 'w')
+
+	for x in passwordlist:
+		dictpass = json.loads(x)
+		if(dictpass['url'] == 'url'):
+			dictpass['hashed password'] = hashed
+		json.dump(dictpass, writeFile)
+	
+	writeFile.close()	
+
 
 def main():
 	userAction = input("do you want to read or write from pasword file: ")
@@ -39,7 +59,7 @@ def main():
 		url = input("what is the url of the website: ")
 		user_normal_password = input('enter password that you want to hash: ')
 		add_to_file(url, user_normal_password)
-	elif(userAction :
+	elif(userAction == 'FIND'):
 		readFile = open('password.json', 'r')
 		website = input('what is the url of the website that you need password for: ')
 		list_pass = readFile.readlines()
