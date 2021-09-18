28572d0222dd90d11519625e687007233f95a012
Bhargav Akula <62191542+bhargavakula01@users.noreply.github.com>
2021-09-18 11:24:04 -0500

    updating code
    Updating "updating_from_file()" and "main()" functions

--- password-hasher/hashpasswords.py
+++ password-hasher/hashpasswords.py
@@ -31,50 +31,57 @@ def read_from_file(url):
 				found = True
 		if(not found):
 			print('url not found')
+
 def updating_from_file(url):
 	readFile = open('password.json','r')
-	inputpass = input("Enter new password that you want to hash")
-	index_password = 0
 	passwordlist = readFile.readlines()
 	readFile.close()
 	
+	inputpass = input("Enter new password that you want to hash")
 	shaobj = sha256()
 	encode = inputpass.encode()
 	hashed = shaobj.hexdigest()
 
 	writefile = open('password.json', 'w')
-
+	url_found = False
 	for x in passwordlist:
 		dictpass = json.loads(x)
 		if(dictpass['url'] == 'url'):
 			dictpass['hashed password'] = hashed
 		json.dump(dictpass, writeFile)
-	
+	if(not url_found):
+		print('Website URL was not found... Please try again')
 	writeFile.close()	
 
-
 def main():
-	userAction = input("do you want to read or write from pasword file: ")
-	if(userAction == "WRITE"):
-		url = input("what is the url of the website: ")
-		user_normal_password = input('enter password that you want to hash: ')
-		add_to_file(url, user_normal_password)
-	elif(userAction == 'FIND'):
-		readFile = open('password.json', 'r')
-		website = input('what is the url of the website that you need password for: ')
-		list_pass = readFile.readlines()
-		list_dict = []
-		for x in list_pass:
-			dict_pass = json.loads(x)
-			list_dict.append(dict_pass)
+	userAction = input("do you want to write, find, or replace from pasword file: ")
+	while(userAction != 'q' or userAction != 'Exit'):
+		if(userAction == "WRITE"):
+			url = input("what is the url of the website: ")
+			user_normal_password = input('enter password that you want to hash: ')
+			add_to_file(url, user_normal_password)
+		elif(userAction == 'FIND'):
+			readFile = open('password.json', 'r')
+			website = input('what is the url of the website that you need password for: ')
+			list_pass = readFile.readlines()
+			list_dict = []
+			for x in list_pass:
+				dict_pass = json.loads(x)
+				list_dict.append(dict_pass)
 
-		# finding password
-		found = False
-		for x in list_dict:
-			if(website == x['url']):
-				print( x['hashed password'])
-				found = True
-		if(not found):
-			print('url not found')
+			# finding password
+			found = False
+			for x in list_dict:
+				if(website == x['url']):
+					print( x['hashed password'])
+					found = True
+			if(not found):
+				print('url not found')
+		else:
+			website_update = input('What website do you want to update password for?: ")
+		
+		userAction = input("do you want to write, find, or replace from pasword file: ")
+		
+		
 if __name__ == "__main__":
 	main()
